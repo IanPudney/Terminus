398b2a37438b63f09619a3897a1cf7785aceff15
ddrocco <keredoccor@gmail.com>
2015-08-15 00:47:27 -0700

    Added timer stuffs!  MainBot moves and stuff.

---
+++ Assets/Scripts/MainBot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainBot : MonoBehaviour {
+	Vector3 StartLocation;
+	Vector3 StopLocation;
+	bool Active;
+
+	// Use this for initialization
+	void Start () {
+		StartLocation = Vector3.zero;
+		Active = false;
+	}
+	
+	void OnEnable() {
+		TimeControl.OnStart += StartAction;
+		TimeControl.OnStop += StopAction;
+	}
+	
+	void OnDisable() {
+		TimeControl.OnStart -= StartAction;
+		TimeControl.OnStop -= StopAction;
+	}
+	
+	void Update() {
+		if (Active) {
+			transform.position = StartLocation + TimeControl.obj.fraction * (StopLocation - StartLocation);
+		}
+	}
+	
+	public void StartAction() {
+		Active = true;
+		StopLocation = StartLocation + Vector3.up;
+	}
+	
+	public void StopAction() {
+		StartLocation = StopLocation;
+		transform.position = StartLocation;
+		Active = false;
+	}
+}
---
+++ Assets/Scripts/TimeControl.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeControl : MonoBehaviour {
+	public delegate void StartAction();
+	public delegate void StopAction();
+	public static event StartAction OnStart;
+	public static event StopAction OnStop;
+	
+	static public TimeControl obj;
+	
+	public int tick = 0;
+	
+	//Handles time controls.
+	float time_since_update = 0;
+	float transition_time = 0.5f;
+	float time_between_steps = 1f;
+	
+	enum state {
+		between,
+		transition
+	};
+	
+	state State = state.between;
+	
+	void Start() {
+		obj = this;
+	}
+	
+	void Update () {
+		time_since_update += Time.deltaTime	;
+		if (State == state.between) {
+			if (time_since_update > time_between_steps) {
+				time_since_update -= time_between_steps;
+				tick += 1;
+				State = state.transition;
+				OnStart();
+			}
+		} else { //state is transitioning
+			if (time_since_update > transition_time) {
+				time_since_update -= time_since_update;
+				State = state.between;
+				OnStop();
+			}
+		}
+	}
+	
+	//Properties are awesome
+	public float fraction {
+		get { return time_since_update / transition_time; }
+	}
+}
