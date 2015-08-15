9845bf69635f80119d5e37848bba96ee4c1ecc1b
ddrocco <keredoccor@gmail.com>
2015-08-15 05:25:32 -0700

    Robots get shoved and can blow up now!!!

--- Assets/Scripts/Doorway.cs
+++ Assets/Scripts/Doorway.cs
@@ -9,13 +9,7 @@ public class Doorway : MonoBehaviour {
 	
 	const float PRIORITY = 2f;
 	
-	public enum CloseDirection {
-		North,
-		South,
-		East,
-		West
-	};
-	public CloseDirection closeDirection = CloseDirection.North;
+	public Vector3 CloseDirection;
 	
 	enum State {
 		Open,
@@ -26,23 +20,20 @@ public class Doorway : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		OpenLocation = transform.position;
-		if (closeDirection == CloseDirection.North) {
-			ClosedLocation = OpenLocation + Vector3.up;
+		ClosedLocation = OpenLocation + CloseDirection;
+		if (CloseDirection == Vector3.up) {
 			DoorBrace = (GameObject)Instantiate(ProtoDict.obj.doorBrace,
 											   transform.position,
 											   Quaternion.Euler(Vector3.zero));
-		} else if (closeDirection == CloseDirection.South) {
-			ClosedLocation = OpenLocation + Vector3.down;
+		} else if (CloseDirection == Vector3.down) {
 			DoorBrace = (GameObject)Instantiate(ProtoDict.obj.doorBrace,
 			                                   transform.position,
 			                                    Quaternion.Euler(Vector3.forward * 180f));
-		} else if (closeDirection == CloseDirection.East) {
-			ClosedLocation = OpenLocation + Vector3.right;
+		} else if (CloseDirection == Vector3.right) {
 			DoorBrace = (GameObject)Instantiate(ProtoDict.obj.doorBrace,
 			                                   transform.position,
 			                                   Quaternion.Euler(Vector3.forward * 270f));
-		} else if (closeDirection == CloseDirection.West) {
-			ClosedLocation = OpenLocation + Vector3.left;
+		} else if (CloseDirection == Vector3.left) {
 			DoorBrace = (GameObject)Instantiate(ProtoDict.obj.doorBrace,
 			                                   transform.position,
 			                                    Quaternion.Euler(Vector3.forward * 90f));
--- Assets/Scripts/MainBot.cs
+++ Assets/Scripts/MainBot.cs
@@ -5,13 +5,14 @@ public class MainBot : MonoBehaviour {
 	Vector3 StartLocation;
 	Vector3 StopLocation;
 	bool Active;
-	
+	Vector3 DoorVector;
 
 	// Use this for initialization
 	void Start () {
 		StartLocation = transform.position;
 		StopLocation = StartLocation;
 		Active = false;
+		DoorVector = Vector3.zero;
 	}
 	
 	//Don't touch this ever.	
@@ -55,5 +56,18 @@ public class MainBot : MonoBehaviour {
 		StartLocation = StopLocation;
 		transform.position = StartLocation;
 		Active = false;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		print ("Fontenot!");
+		Doorway door = other.gameObject.GetComponent<Doorway>();
+		if (door) {
+			if (DoorVector + door.CloseDirection == Vector3.zero) {
+				Time.timeScale = 0.0f;
+			} else if (DoorVector == Vector3.zero) {
+				DoorVector = door.CloseDirection;
+				StopLocation += DoorVector;
+			}
+		}
 	}
 }
