045720b1a9e7584f712152e658eed7c54ad5d922
ddrocco <keredoccor@gmail.com>
2015-08-16 00:22:10 -0700

    Level 3 is done!

--- Assets/Scripts/LevelTransition.cs
+++ Assets/Scripts/LevelTransition.cs
@@ -13,8 +13,18 @@ public class LevelTransition : MonoBehaviour {
 	public void StopAction() {
 		foreach (MainBot bot in ObjectDict.obj.robots) {
 			if (Vector3.Distance(bot.transform.position, transform.position) < 0.1f) {
-				print ("Victory!");
+				Application.LoadLevel(LevelMap());
 			}
 		}
+	}
+	
+	string LevelMap() {
+		switch (Application.loadedLevelName) {
+			case "Level2": return "Level3";
+			case "Level3": return "Level4";
+			case "Level4": return "Level5";
+			case "Level5": return "Level2";
+		}
+		return "";
 	}
 }
