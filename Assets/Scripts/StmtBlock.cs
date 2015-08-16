278d4ebd147ef09ae15a49c26db30a9ab0fa94c3
ddrocco <keredoccor@gmail.com>
2015-08-15 20:18:26 -0700

    Buttons snap and work and stuff and Ian our game is going to be incredible I'm so excited can you feel how great this is????

--- Assets/Scripts/StmtBlock.cs
+++ Assets/Scripts/StmtBlock.cs
@@ -9,12 +9,13 @@ public class StmtBlock : Statement {
 	base.Start ();
     GameObject initialPlaceholder = Instantiate(ProtoDict.obj.placeholder, transform.position, transform.rotation) as GameObject;
     initialPlaceholder.transform.SetParent(this.transform);
+	initialPlaceholder.transform.localScale = Vector3.one;
     initialPlaceholder.GetComponent<Placeholder> ().OnReplace += ReplacePlaceholder;
 
 	indentationHint = Instantiate(ProtoDict.obj.indentationHint, transform.position, transform.rotation) as GameObject;
 	indentationHint.transform.SetParent(this.transform);
 	indentationHint.transform.localPosition = new Vector3(-2f, 0f, 0f);
-	indentationHint.transform.localScale = new Vector3(1, 1, 1);
+	indentationHint.transform.localScale = Vector3.one;
 	indentationHint.GetComponent<Image>().color = GetComponent<Image>().color;
     Layout ();
   }
@@ -32,9 +33,10 @@ public class StmtBlock : Statement {
 		RecursiveLayout ();
   }
 
+	//RETURNS: Total height of object with all children.
   public override float Layout() {
-    Vector3 nextPosition = new Vector3 (1f, -transform.localScale.y, 0);
-    Transform last = null;
+  	//Position immediately beneath
+    Vector3 nextPosition = new Vector3 (1f, -1f, 0);
     foreach (Transform child in transform) {
       ProgBlock block = child.GetComponent<ProgBlock>();
       if (!block) {
@@ -44,13 +46,9 @@ public class StmtBlock : Statement {
       Vector3 blockPosition = block.transform.localPosition;
       block.transform.localPosition = nextPosition;
       nextPosition.y -= height;
-      last = child;
     }
-    //nextPosition = last.transform.localPosition;
-    //nextPosition.x = 0.0f;
-    //last.transform.localPosition = nextPosition;
-		indentationHint.transform.localScale = Vector3.one;
-		indentationHint.transform.localPosition = new Vector3(-2, -1, 0);
+	indentationHint.transform.localScale = Vector3.one;
+	indentationHint.transform.localPosition = new Vector3(-2f, -1f, 0);
 
     return -nextPosition.y;
   }
