4f3e6c08cf0de0a6251eb3cf179c99ad98bbc503
ddrocco <keredoccor@gmail.com>
2015-08-16 08:48:59 -0700

    Killing it with the updates to Placeholders.  Snapping now, sparkling next.

--- Assets/Scripts/Placeholder.cs
+++ Assets/Scripts/Placeholder.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Placeholder : Statement {
 	//this statement does nothing
@@ -18,7 +19,9 @@ public class Placeholder : Statement {
 	static public Placeholder GetClosestPlaceholder(Transform targetTransform) {
 		Placeholder pMin = null;
 		float minDist = float.MaxValue;
-		Vector3 currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//Vector3 currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Bounds targetBounds = targetTransform.GetComponent<BoxCollider>().bounds;
+		
 		foreach (Object obj in FindObjectsOfType<Placeholder>()) {
 			Placeholder holder = (Placeholder) obj;
 			Bounds objBounds = holder.GetComponent<BoxCollider>().bounds;
@@ -26,14 +29,23 @@ public class Placeholder : Statement {
 			if(holder.transform.parent == targetTransform) {
 				continue;
 			}
-			float dist = Vector2.Distance(holder.transform.position, currentPos);
-			if (dist < minDist &&
-					currentPos.x < objBounds.max.x &&
-					currentPos.y < objBounds.max.y &&
-					currentPos.x > objBounds.min.x &&
-					currentPos.y > objBounds.min.y) {
-				minDist = dist;
-				pMin = holder;
+			float dist = Vector2.Distance(holder.transform.position, targetBounds.center);
+			if (dist < minDist) {
+				List<Vector2> corners = new List<Vector2>();
+				corners.Add (targetBounds.max);
+				corners.Add (targetBounds.min);
+				corners.Add(new Vector2(targetBounds.max.x, targetBounds.min.y));
+	            corners.Add(new Vector2(targetBounds.min.x, targetBounds.max.y));
+	            foreach (Vector2 corner in corners) {
+	            	if (
+							corner.x < objBounds.max.x &&
+							corner.y < objBounds.max.y &&
+							corner.x > objBounds.min.x &&
+							corner.y > objBounds.min.y) {
+						minDist = dist;
+						pMin = holder;
+					}
+				}		
 			}
 		}
 		foreach (BlockBank bank in FindObjectsOfType<BlockBank>()) {
@@ -43,12 +55,12 @@ public class Placeholder : Statement {
 			}
 			//Check children, etc
 			Bounds bankBounds = bank.GetComponent<BoxCollider>().bounds;
-			float dist = Vector2.Distance(bank.transform.position, currentPos);
+			float dist = Vector2.Distance(bank.transform.position, targetBounds.center);
 			if (dist < minDist &&
-				    currentPos.x < bankBounds.max.x &&
-				    currentPos.y < bankBounds.max.y &&
-				    currentPos.x > bankBounds.min.x &&
-				    currentPos.y > bankBounds.min.y) {
+				    targetBounds.center.x < bankBounds.max.x &&
+				    targetBounds.center.y < bankBounds.max.y &&
+				    targetBounds.center.x > bankBounds.min.x &&
+				    targetBounds.center.y > bankBounds.min.y) {
 				bank.numLeft += 1;
 				bank.GetComponentInChildren<TextMesh>().text = ("x" + bank.numLeft);
 				Destroy (targetTransform.GetComponentInChildren<Image>().gameObject);
